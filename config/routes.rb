1482ba4c9b26d13d08fdd1d7e1fa89b502aa0f80
saikat <saikat.0291@gmail.com>
2016-10-14 20:59:05 +0530

    added card checking logic

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
   get '/cart/clearDelQtyById/:product_id' => 'cart#clearDelQtyById'
   get '/contact' => 'esite_controllers#contact'
   get '/home' => 'esite_controllers#index'
-  #get '/users/sign_out' => 'esite_controllers#index'
   get '/error' => 'esite_controllers#E404'
   get '/blog' => 'esite_controllers#blog'
   get '/checkout' => 'cart#checkout'
