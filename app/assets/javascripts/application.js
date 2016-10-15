76fe4e20c40d0cd686a36d3aaa50b7a27b1d0199
saikat <saikat.0291@gmail.com>
2016-10-15 12:36:23 +0530

    try to solve $ sign for server not responding

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //require "open-uri"
 //= require jquery_ujs
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap.min 
 //= require turbolinks
 //= require_tree .
 	
